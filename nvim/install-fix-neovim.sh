c70f24bc2efc678961f2f62c44f477061930f217
Delayless <Delayless@gmail.com>
2020-11-02 06:48:29 +0800

    Added dwm.desktop Entry for lightDM. Added neovim(nvim).

---
+++ nvim/install-fix-neovim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo pacman -S neovim --noconfirm
+pip3 install neovim --upgrade
+# vimtex-faq-neovim
+pip3 install neovim-remote
+# nodejs
+sudo npm install -g neovim
+# checkhealth
+pip3 uninstall pynvim neovim
+pip3 install pynvim
+pip3 install neovim
+python2 -m pip install neovim
+python2 -m pip install pynvim
