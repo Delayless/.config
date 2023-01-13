f485c51de270dee5def76dcbcd45741b20331903
Delayless <Delayless@gmail.com>
2023-01-13 10:58:42 +0800

    perf: picom configuration; .tmux.conf add variable resurrect-dir

--- install/picom.sh
+++ install/picom.sh
@@ -3,11 +3,11 @@
 git clone https://github.com/Delayless/picom /tmp/picom
 cd /tmp/picom
 git submodule update --init --recursive
-git checkout implement-window-animations
 
 sudo pacman -S ninja meson cmake libev uthash libconfig --noconfirm
 
 rm -rf build
+# LDFLAGS="-L/usr/local/lib" CPPFLAGS="-I/usr/local/include" meson --buildtype=release . build
 meson --buildtype=release . build
 ninja -C build
 sudo ninja -C build install
